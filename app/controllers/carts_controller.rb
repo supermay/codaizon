50e65eb4e669abfd908dcb8d48ef8966fad3a8f0
ISMAIL YASSIN <YISMAIL299@GMAIL.COMssh-keygen -t rsa -C ISMAIL>
2017-07-11 15:42:21 +0200

    Added create methos to carts controller

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,15 @@
+class CartsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def create
+    @cart = current_user.carts.create(cart_params)
+
+  end
+  private
+
+  def cart_params
+    params.require(:cart).permits(:product_id, :total_price)
+
+  end
+end
