7dc72ba9512f21725002d6887731ab27c26e2a16
Chuday <chuday28@gmail.com>
2017-07-11 18:20:25 +0200

    testing model product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,16 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe "Association with carts through orders"  do
-
-   let!(:product) { create :product }
-   let!(:user) { create :user }
-   let!(:cart) { create :cart, user: user}
-   let!(:order) { create :order, product: product, cart: cart }
-
-    it "has orders and products" do
-      cart1 = user.carts.new()
-      expect(cart.orders).to include(product)
-
-    end
+   it { should have_many(:carts).through(:orders)}
   end
 end
